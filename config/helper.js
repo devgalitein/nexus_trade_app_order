62e22e7b3f92bf05a43abf1a83fe82a7f8f0252d
Sagar Panchal <dev.galitein@gmail.com>
2023-10-11 13:36:59 +0530

    change a lot size and make changes in lot selling logic.

--- config/helper.js
+++ config/helper.js
@@ -3,42 +3,70 @@ const db = require("../models/index");
 const { getQuotes } = require("../config/kiteConnect");
 const moment = require("moment");
 const watchListModel = db.watchList;
-const watchLists_backup = db.watchLists_backup;	
+const watchLists_backup = db.watchLists_backup;
 const getOrdersLegs = async () => {
-	return new Promise((resolve, reject) => {
-		sql.query(
-			`SELECT DISTINCT legs.*,setting.total_quantity,setting.entry_time,setting.order_type,setting.exit_time,setting.auto_repeat,setting.allow_order FROM legs LEFT JOIN orders on legs.id=orders.leg_id LEFT JOIN setting ON legs.setting_id=setting.id WHERE legs.active_leg=0 AND orders.created_at != '${moment().format(
-				"YYYY-MM-DD"
-			)}' OR orders.id IS NULL OR (orders.exit_date_time IS NOT NULL AND setting.auto_repeat=1)
+  return new Promise((resolve, reject) => {
+    sql.query(
+      `SELECT DISTINCT legs.*,setting.total_quantity,setting.entry_time,setting.order_type,setting.exit_time,setting.auto_repeat,setting.allow_order FROM legs LEFT JOIN orders on legs.id=orders.leg_id LEFT JOIN setting ON legs.setting_id=setting.id WHERE legs.active_leg=0 AND orders.created_at != '${moment().format(
+        "YYYY-MM-DD"
+      )}' OR orders.id IS NULL OR (orders.exit_date_time IS NOT NULL AND setting.auto_repeat=1)
       `,
-			(err, res) => {
-				if (err) {
-					reject(0);
-				} else {
-					resolve(res);
-				}
-			}
-		);
-	});
+      (err, res) => {
+        if (err) {
+          reject(0);
+        } else {
+          resolve(res);
+        }
+      }
+    );
+  });
 };
 
 const getNiftyPrice = async () => {
-	code = ["NSE:NIFTY BANK"];
-	if (code) {
-		const quotes = await getQuotes(code);
-		console.log("codecodecodecodecodecodecode---->>>>", quotes);
-		return quotes?.["NSE:NIFTY BANK"]?.last_price;
-	}
+  code = ["NSE:NIFTY BANK"];
+  if (code) {
+    const quotes = await getQuotes(code);
+    console.log("codecodecodecodecodecodecode---->>>>", quotes);
+    return quotes?.["NSE:NIFTY BANK"]?.last_price;
+  }
 };
 const getdays = (date) => {
-	let year = date.getFullYear().toString().substr(-2);
-	let day = date.getDate();
-	day = day < 10 ? "0" + day : day;
-	let month = date.getMonth() + 1;
-	return { day, year, month };
+  let year = date.getFullYear().toString().substr(-2);
+  let day = date.getDate();
+  day = day < 10 ? "0" + day : day;
+  let month = date.getMonth() + 1;
+  return { day, year, month };
+};
+
+const getThursdayDate = () => {
+  const currentDate = new Date();
+
+  // Get the last day of the current month
+  const lastDayOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1,
+    0
+  );
+
+  // Calculate the last Thursday date of the current month
+  const lastThursday = new Date(lastDayOfMonth);
+  lastThursday.setDate(
+    lastThursday.getDate() - ((lastDayOfMonth.getDay() + 7 - 4) % 7)
+  );
+
+  // Calculate the second last Thursday date of the current month
+  const secondLastThursday = new Date(lastThursday);
+  secondLastThursday.setDate(secondLastThursday.getDate() - 7);
+
+  // Check if the current date falls between the two dates
+  const isBetween =
+    currentDate >= secondLastThursday && currentDate <= lastThursday;
+
+  return isBetween;
 };
 module.exports = {
-	getOrdersLegs,
-	getNiftyPrice,
-	getdays,
+  getOrdersLegs,
+  getNiftyPrice,
+  getdays,
+  getThursdayDate,
 };
